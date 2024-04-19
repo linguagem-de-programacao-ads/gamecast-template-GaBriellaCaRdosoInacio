ddc4c11b29968f84bf2bcd1215f233ef96ec75e8
GaBriellaCaRdosoInacio <gabriella.inacio@sptech.school>
2024-04-18 21:21:10 -0300

    cards puxando da API e atualizacao de responsividade cards

--- js/buscar.js
+++ js/buscar.js
@@ -10,8 +10,31 @@ async function buscar(){
 
     //percorre os elementos da resposta, passa por cada item que percorre a respostaAgenda (array)
     cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
-        return "TESTE "
-    }); 
+        return `
+        <div class="cardItem">
+        <div class="dataGame"> 
+            <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo}
+        </div>
+        <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+        <div class="descricao">
+            <p> <strong>${itemAgenda.nome}</strong> </p>
+            <p>${itemAgenda.descricao} </p>
+            <p><strong>Gamers: </strong></p>
+
+            <div class="gamers">
+                ${
+                    itemAgenda.gamers.map((gamerItem) =>{
+                        return `<div class="gamerItem">@${gamerItem}</div>`
+                    }).join(' ')
+                }
+               </div> 
+        </div>  
+        <div class="assistir">
+                <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+        </div>
+    </div>
+        `
+    }).join(' '); 
 
 }
 console.log("antes de buscar")
